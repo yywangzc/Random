f868a11bcd9e88b8bb739a2a62ba3fb487021f11
YY Wang <yywangzc@gmail.com>
2023-06-18 20:50:00 +0800

    version_2

--- Random.py
+++ Random.py
@@ -1,13 +1,15 @@
 import random
 r = random.randint(1, 100)
+t = 0
 while True:
 	g = input('Please guess a number between 1~100 : ')
 	g = int(g)
+	t = t + 1 # t += 1
 	if g == r:
-		print('Guessed Right!')
+		print('Guessed Right! Guess', t, 'times')
 		break
 	else:
 		if g > r:
-			print('Guess towards the lower number')
+			print('Guess towards the lower number, guess', t, 'times')
 		elif g < r:
-			print('Guess towards the higher number') 
+			print('Guess towards the higher number, guess', t, 'times') 
